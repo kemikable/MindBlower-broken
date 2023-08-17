406555ba784cdbc8a1f7ebc7e26635605d2fedc8
Kamran Aghayev <124082311+kemikable@users.noreply.github.com>
2023-08-18 01:41:15 +0400

    0.0.1

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Test gggffyfkj</Text>
+      {/* <StatusBar style="auto" /> */}
+      <StatusBar hidden />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aad',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+
+  },
+  text: {
+    color: "yellow"
+  }
+});
