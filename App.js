1d8d8f09622914ce6a4ed7ce69eeb3b345baacd1
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-08-18 21:23:01 +0400

    adding bruttons

--- App.js
+++ App.js
@@ -5,22 +5,40 @@ import {
   ImageBackground,
   SafeAreaView,
   Image,
-  Text,
+  Button,
+  Alert,
+  View,
+  Text
 } from "react-native";
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+
       <ImageBackground
+
         source={require("./img/bg.png")} // Замените путем на реальный путь к вашему изображению
         style={styles.imageBackground}
       >
+
+        <View style={styles.buttonRight}>
+          {/* <Button title=" More" onPress={() => Alert.alert("Pressed")} /> */}
+        </View>
+
+        <View style={styles.buttonLeft}>
+          {/* <Button title=" More" onPress={() => Alert.alert("Pressed")} /> */}
+        </View>
         <Image
           source={require("./img/logomb.png")} // Замените путь на реальный путь к вашему изображению
-          style={{ width: 200, height: 200 }} // Установите ширину и высоту изображения
+          style={{ width: 350, height: 200, bottom: 270 }} // Установите ширину и высоту изображения
         />
+        <View style={styles.buttonStart}>
+          <Text style={styles.textStart}>Start</Text>
+          {/* <Button title=" More" onPress={() => Alert.alert("Pressed")} /> */}
+        </View>
       </ImageBackground>
-      <Text>FDFDFDFD</Text>
+
 
       <StatusBar style="auto" />
     </SafeAreaView>
@@ -28,6 +46,35 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  buttonRight: {
+    width: 40,
+    height: 40,
+    bottom: 240,
+    left: 145,
+    backgroundColor: "grey",
+    borderRadius: 50
+  },
+  buttonLeft: {
+    width: 40,
+    height: 40,
+    bottom: 285,
+    right: 145,
+    backgroundColor: "grey",
+    borderRadius: 50
+  },
+  buttonStart: {
+    flex: 1,
+    justifyContent: "center",
+
+    width: 270,
+    height: 70,
+
+    backgroundColor: "grey",
+    borderRadius: 50
+  },
+  textStart: {
+    textAlign: "center",
+  }
   container: {
     flex: 1,
   },
