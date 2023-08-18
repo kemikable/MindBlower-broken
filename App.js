16d2ce0c32d1ecd38d305afd20ac728b3f444f31
Adilek96 <ashurovadil96@gmail.com>
2023-08-18 20:15:26 +0400

    bg

--- App.js
+++ App.js
@@ -1,26 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  Image,
+  Text,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Test gggffyfkj</Text>
-      {/* <StatusBar style="auto" /> */}
-      <StatusBar hidden />
-      <Button title={"Some text"} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require("./img/bg.png")} // Замените путем на реальный путь к вашему изображению
+        style={styles.imageBackground}
+      >
+        <Image
+          source={require("./img/logomb.png")} // Замените путь на реальный путь к вашему изображению
+          style={{ width: 200, height: 200 }} // Установите ширину и высоту изображения
+        />
+      </ImageBackground>
+      <Text>FDFDFDFD</Text>
+
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aad',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-
   },
   text: {
-    color: "yellow"
-  }
+    color: "yellow",
+  },
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover", // Изменяйте в зависимости от вашего предпочтения (cover, contain, stretch и др.)
+    justifyContent: "center", // Выравнивание по центру по вертикали
+    alignItems: "center", // Выравнивание по центру по горизонтали
+  },
 });
