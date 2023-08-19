9a7f9864eea7bfde0c3cf0e7f551f738a476b16a
Kamran Aghayev <aghayev.kamran@outlook.com>
2023-08-20 03:18:05 +0400

    question modal and credits added

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import {
   ImageBackground,
   SafeAreaView,
   View,
+  Modal,
+  Text,
+  Linking,
   StyleSheet,
   Image,
   TouchableWithoutFeedback,
@@ -13,6 +16,7 @@ import {
 const App = () => {
   const [hiddenBtn, setHiddenBtn] = useState(-400);
   const [modalVisible, setModalVisible] = useState(false);
+
   const menuOnPress = () => {
     if (hiddenBtn === 0) {
       setHiddenBtn(-100);
@@ -21,9 +25,17 @@ const App = () => {
     }
   };
 
+  const openModalQuestion = () => {
+    setModalVisible(true);
+  };
+
+  const closeModalQuestion = () => {
+    setModalVisible(false);
+  };
+
   return (
     <ImageBackground
-      source={require("./img/bg.png")} // Замените путь на реальный путь к вашему изображению
+      source={require("./img/bg.png")}
       style={styles.backgroundImage}
       resizeMode="cover"
     >
@@ -33,24 +45,43 @@ const App = () => {
           <View style={styles.topBtnBox}>
             <TouchableWithoutFeedback onPress={menuOnPress}>
               <Image
-                source={require("./img/menuBtn.png")} // Замените путем на реальный путь к вашему изображению
+                source={require("./img/menuBtn.png")}
                 style={styles.menuImage}
                 resizeMode="contain"
               />
             </TouchableWithoutFeedback>
-            <TouchableWithoutFeedback onPress={menuOnPress}>
+
+            <TouchableWithoutFeedback onPress={openModalQuestion}>
               <Image
-                source={require("./img/questionBtn.png")} // Замените путем на реальный путь к вашему изображению
+                source={require("./img/questionBtn.png")}
                 style={[
                   styles.questionImage,
-                  { right: hiddenBtn }, // Используем hiddenBtn в стиле
+                  { right: hiddenBtn },
                 ]}
                 resizeMode="contain"
               />
             </TouchableWithoutFeedback>
+
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={closeModalQuestion}
+            >
+              <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                  <Text>Игра для мозговых штурмов! Mindblower - взрывная интеллектуальная развлекалка!</Text>
+                  <TouchableWithoutFeedback onPress={closeModalQuestion}>
+
+                    <Text style={styles.closeButton}>Закрыть</Text>
+                  </TouchableWithoutFeedback>
+                </View>
+              </View>
+            </Modal>
+
             <TouchableWithoutFeedback onPress={menuOnPress}>
               <Image
-                source={require("./img/settingsBtn.png")} // Замените путем на реальный путь к вашему изображению
+                source={require("./img/settingsBtn.png")}
                 style={[
                   styles.settingsImage,
                   { right: hiddenBtn }, // Используем hiddenBtn в стиле
@@ -63,7 +94,7 @@ const App = () => {
         <View style={styles.box2}>
           <View style={styles.logoContainer}>
             <Image
-              source={require("./img/logomb.png")} // Замените путем на реальный путь к вашему изображению
+              source={require("./img/logomb.png")}
               style={styles.logoImage}
               resizeMode="contain"
             />
@@ -72,12 +103,29 @@ const App = () => {
         <View style={styles.box3}>
           <View style={styles.startButton}>
             <Image
-              source={require("./img/startBtn.png")} // Замените путем на реальный путь к вашему изображению
+              source={require("./img/startBtn.png")}
               style={styles.startBtnIimage}
               resizeMode="contain"
             />
+            <View style={styles.overlay}></View>
           </View>
         </View>
+        <Text style={styles.text}>
+          Created with honor by{' '}
+          <Text
+            style={styles.linkText}
+            onPress={() => Linking.openURL('https://github.com/kemikable')}
+          >
+            @kemikable
+          </Text>{' '}
+          and{' '}
+          <Text
+            style={styles.linkText}
+            onPress={() => Linking.openURL('https://github.com/adilek96')}
+          >
+            @adilek96
+          </Text>
+        </Text>
       </SafeAreaView>
     </ImageBackground>
   );
@@ -171,6 +219,41 @@ const styles = StyleSheet.create({
     opacity: 0.8,
     width: 50,
     height: 50,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'lightblue',
+    padding: 20,
+    borderRadius: 10,
+  },
+  closeButton: {
+    textAlign: 'right',
+    marginTop: 10
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject, // Занимает всю доступную площадь
+    backgroundColor: 'rgba(144, 238, 144, 0.3)', // Прозрачный  цвет
+    borderRadius: 100, // Опционально, если нужно округлить задний фон
+    zIndex: -1, // Отправляет элемент на задний план
+    borderWidth: 1,
+    borderColor: 'transparent', // Чтобы избежать граничных конфликтов
+  },
+  text: {
+    textDecorationLine: "underline",
+    color: "white",
+    fontSize: 16,
+    textAlign: 'left',
+  },
+  linkText: {
+    textDecorationLine: "underline",
+    fontWeight: "bold",
+    fontSize: 20,
+    textDecorationLine: 'underline',
   },
 });
 
